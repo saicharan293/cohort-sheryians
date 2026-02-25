8e1fa5b8183408a9bb512a95c6a9a044f6441b3e
saicharan293 <2493saicharan@gmail.com>
2026-02-25 22:36:07 +0530

    intro to js

---
+++ 8-JavaScript/script.js
@@ -0,0 +1,10 @@
+// javascript: a scripting language (a programming language that interprets at runtime rather than being compiled into machine code)
+
+// interpreter is a s/w that excutes code line by line
+
+// ex of scripting languages: Js, Python, powershell, ruby, php
+
+// latest js is es6 (ecma script 6 - due to collision among different versions of js)
+
+// Importance: 
+// animation, interactive, engagement creation
