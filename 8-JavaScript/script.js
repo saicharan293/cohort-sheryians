e089432e5a92c23662681343075badde27b5f3c8
saicharan293 <2493saicharan@gmail.com>
2026-02-27 00:33:16 +0530

    slight updates in js

--- 8-JavaScript/script.js
+++ 8-JavaScript/script.js
@@ -1,10 +1,22 @@
-// javascript: a scripting language (a programming language that interprets at runtime rather than being compiled into machine code)
+// javascript: a scripting language used to create dynammic and interactive web pages
+// It is interpreted at runtime rather than being compiled into machine code
 
-// interpreter is a s/w that excutes code line by line
+// Interpreter is a s/w that excutes code line by line
 
-// ex of scripting languages: Js, Python, powershell, ruby, php
+// ex of scripting languages: 
+// - Javascript, 
+// - Python,
+// - powershell,
+// - ruby,
+// - php
 
 // latest js is es6 (ecma script 6 - due to collision among different versions of js)
-
+// Modern JavaScript:
+// ES6 (ECMAScript 2015) introduced major improvements such as:
+// - let & const
+// - Arrow functions
+// - Template literals
+// - Classes
+// - Modules
 // Importance: 
 // animation, interactive, engagement creation
