df3f305bb5c856f7671e01f1a7c1b6c7fc73c261
saicharan293 <2493saicharan@gmail.com>
2026-03-13 13:24:26 +0530

    questions on the array and its methods

--- 8-JavaScript/Arrays.js
+++ 8-JavaScript/Arrays.js
@@ -144,3 +144,113 @@
 // to copy the values in a new array 
 // without referencing to the parent array
 // let arr2 = [...arr];
+
+
+
+//------------- QUESTIONS ---------------------
+// ----------Create an array with 3 fruits and print second fruit
+
+// let fruits = ['Apple', 'Banana','Mango'];
+
+// console.log(fruits[1]);
+
+//------------ Add "Mango" at the end and "Pineapple" at the beginning of this array--------
+
+// let fruits = ["Apple","Banana"];
+
+// fruits.push("Mango");
+// fruits.unshift("Pineapple");
+
+//----------- Replace "Banana" with "Kiwi"-------------------
+
+// let fruits = ["Apple","Banana"];
+// fruits[2]="Kiwi";
+// fruits.pop();
+// fruits.push("Kiwi")
+
+// -------------- Remove the last item from this array using a method: --------
+
+// let numbers = [1, 2, 3, 4];
+// numbers.pop();
+
+//--------------- Insert "Red" and "Blue" at index1 in this array --------
+
+// let colors = ["Green", "Yellow"];
+// colors.splice(1, 0, "Red", "Blue");
+
+//--------------- Extract only the middle 3 elements from this array ------------
+
+// let items = [1, 2, 3, 4, 5, 6];
+// let newItems = items.slice(1, 4);
+
+//-------------- Sort the array alphabetically and then reverse it ---------------
+
+// let names = ["Zara", "Arjun", "Mira", "Bhavya"];
+// names.sort().reverse();
+
+//-------------------use map to square each number ---------------------------
+
+// let arr =[1, 2, 3, 4];
+// let sqarr = arr.map(function(e){
+//     return e*e;
+// })
+
+//-----------------use filter to get numbers > 10 -----------------------
+
+// let arr = [5, 12, 8, 20, 31];
+// let newarr = arr.filter((val)=>{
+//     return val > 10
+// })
+
+// ----------------- use reduce to find the sum ----------------------
+
+// let arr = [10, 20, 30];
+// let res = arr.reduce(function(accumulator, val){
+//     return accumulator+val;
+// },0)
+
+//----------------- get first number less than 10 -----------------------
+
+// let arr = [12, 15, 3, 8, 20];
+// let first = arr.find(function(val){
+//     return val<10;
+// })
+
+// ---------------- check if any student has scored below 35 -------------
+
+// let arr = [45, 60, 28, 90];
+
+// let ans = arr.some(function(val){
+//     return val<35;
+// })
+
+//------------------check if all are even numbers ----------------------
+
+// let arr = [2, 4, 6, 8, 10];
+// let ans = arr.every(function(val){
+//     return val%2===0;
+// })
+
+// --------------- get first and last name -----------------
+
+// let fullname = ["sai", "Amudala"];
+// let [first, last] = fullname;
+
+
+//---------------- merge two arrays ------------------
+
+// let a = [1, 2];
+// let b = [3, 4];
+
+// let c = [...a, ...b];
+
+
+//------------------- add "India" to the start of this array using spread----------
+
+// let countries = ["USA","UK"];
+// countries = ["India", ...countries];
+
+//---------------------clone the array but not reference  -------
+
+// let arr = [1, 2, 3];
+// let arr2 = [...arr];
