727674ba62166cce6b6a2eb375b8320a6a1084b2
saicharan293 <2493saicharan@gmail.com>
2026-03-13 17:52:23 +0530

    intro to objects

---
+++ 8-JavaScript/Object.js
@@ -0,0 +1,37 @@
+//------Object-------
+//----------- Object is a key value pair set and represented as { } ----
+// easy method
+
+// let obj = {
+//     name: "sai",
+//     age : 27, 
+//     email : "sai@gmail.com"
+// }
+
+// text book method
+// let obj2 = new Object();
+
+
+//---- accessing method ----
+// console.log(obj.name);
+// or
+// console.log(obj['name']);
+
+//---- delete property -----
+// delete obj.age;
+
+//----- Nested objects ------
+
+// let obj= {
+//     name: "sai",
+//     socials: {
+//         instagram: "insta",
+//         facebook: "fb"
+//     }
+// }
+
+// console.log(obj.socials.instagram);
+
+
+
+
