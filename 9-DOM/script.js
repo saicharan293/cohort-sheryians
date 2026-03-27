ddf822e8a4af993ab6bf482f511ad4a983158082
saicharan293 <2493saicharan@gmail.com>
2026-03-27 16:35:25 +0530

    Custom Cursor Project

--- 9-DOM/script.js
+++ 9-DOM/script.js
@@ -11,8 +11,8 @@
 // keypress
 
 let main = document.querySelector("main");
-
+let cursor = document.querySelector("#cursor");
 main.addEventListener("mousemove", function(e){
-    console.log(e);
-    
+    cursor.style.left = e.x+'px';
+    cursor.style.top = e.y+'px';
 })
