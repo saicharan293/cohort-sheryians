ae9092e2df4ee7cfcc6d175a162b8b9167b8251a
saicharan293 <2493saicharan@gmail.com>
2026-04-09 20:57:04 +0530

    matrix text deep dive

--- 9-DOM/MatrixText/script.js
+++ 9-DOM/MatrixText/script.js
@@ -3,20 +3,27 @@ const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const text = para.innerText;
 
 let iteration = 0;
+let interval = null; 
 
 function randomText(){
     const randomString = text.split('').map((char, idx)=>{
-        if(idx < iteration){
-            return char;
-        }
+        if(idx < iteration) return char;
+        // if(idx >= text.length - iteration){
+        //     return char;
+        // }
         return characters.split('')[Math.floor(Math.random()*52)];
     }).join("");
 
     para.innerText = randomString;
 
     iteration += 0.5;
+
+    if(iteration >= text.length) clearInterval(interval);
 }
 
 para.addEventListener("mouseenter",()=>{
-    let interval = setInterval(randomText,30);
+    iteration = 0;
+    clearInterval(interval);
+    para.innerText = text;
+    interval = setInterval(randomText,30);
 })
