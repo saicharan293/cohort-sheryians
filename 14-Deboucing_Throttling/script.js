ab7385f0851f2f0502c344f094335943e20dcc3c
saicharan293 <2493saicharan@gmail.com>
2026-04-26 18:57:53 +0530

    Debouncing Intro

---
+++ 14-Deboucing_Throttling/script.js
@@ -0,0 +1,20 @@
+
+// debouncing and throtting
+
+// Debounce → “I will only act after things have been quiet for some time. if not timer resets”
+// use case: to avoid too many rapid requests
+
+// Throttle → “While user is active, I run at a fixed rate”
+
+function debouce(fn, delay){
+    let timer;
+    return function(){
+        clearTimeout(timer)
+        timer = setTimeout(fn, delay);
+    }
+}
+
+document.querySelector(".search").addEventListener("input", 
+    debouce(function(){
+        console.log("chala")
+    }, 400));
