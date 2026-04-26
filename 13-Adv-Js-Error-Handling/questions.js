10d6172b2b0e726f310bc7802032733a2f3e5976
saicharan293 <2493saicharan@gmail.com>
2026-04-26 16:32:41 +0530

    fetch for weather using async/await along with try...catch

--- 13-Adv-Js-Error-Handling/questions.js
+++ 13-Adv-Js-Error-Handling/questions.js
@@ -1,10 +1,19 @@
 
 
-function getWeather(city){
-    let apiKey = '3e2da34937e7f4fdbb2e839856f80d30';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    .then((raw)=> raw.json())
-    .then((res)=> console.log(res.weather[0].main));
+async function getWeather(city){
+    try {
+        let apiKey = '3e2da34937e7f4fdbb2e839856f80d30';
+        let raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+
+        if(!raw.ok){
+            throw new Error("City not found, try Something else");
+        }
+
+        let realData = await raw.json();
+        console.log(realData)
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-getWeather("Kurnool");
+getWeather("Phil");
