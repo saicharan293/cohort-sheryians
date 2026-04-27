e9d3b1655995da4808e2aa762f4504c10c4f0054
saicharan293 <2493saicharan@gmail.com>
2026-04-27 21:41:28 +0530

    started with todo page

--- 15-Dom-Project/script.js
+++ 15-Dom-Project/script.js
@@ -1,7 +1,21 @@
-let allElems = document.querySelectorAll(".elem");
 
-allElems.forEach(function(elem){
-    elem.addEventListener("click", function(){
-        document.querySelectorAll('.fullElem')[elem.id].style.display = 'block';
+function openFeatures(){
+    let allElems = document.querySelectorAll(".elem");
+    let allBackButtons = document.querySelectorAll(".fullElem .back");
+    let allFullElems = document.querySelectorAll('.fullElem');
+
+    allElems.forEach(function(elem){
+        elem.addEventListener("click", function(){
+            allFullElems[elem.id].style.display = 'block';
+        })
     })
-})
+
+    allBackButtons.forEach(function(back) {
+        back.addEventListener("click", function(){
+            console.log(back.id);
+            allFullElems[back.id].style.display = 'none';
+        })
+    });
+}
+
+openFeatures();
