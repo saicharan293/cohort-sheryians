e754e1901291531118a817c90814c5017cd0017c
saicharan293 <2493saicharan@gmail.com>
2026-04-28 02:25:07 +0530

    done upto form submission

--- 15-Dom-Project/script.js
+++ 15-Dom-Project/script.js
@@ -19,3 +19,74 @@ function openFeatures(){
 }
 
 openFeatures();
+
+let form = document.querySelector(".todo-container .addTask form");
+
+let allTask = document.querySelector(".todo-container .allTask");
+let taskInput = document.querySelector(".addTask form input");
+let taskArea = document.querySelector(".addTask form textarea");
+
+let taskImp = document.querySelector(".addTask .mark input#check");
+
+let currenTasks = [
+    {
+        task: 'Mandir jao',
+        details: 'Hanuman',
+        imp: true
+    },
+    {
+        task: 'Coding shuru',
+        details: 'Cohort 2',
+        imp: true
+    },
+    {
+        task: 'Lunch kar',
+        details: 'Food',
+        imp: false
+    },
+]
+
+
+
+function renderTask(){
+    var sum = '';
+    currenTasks.forEach(function(e){
+        sum += `
+        <div class="task">
+            <h5>${e.task} <span class="${e.imp}">imp</span></h5>
+            <button>Mark as Completed</button>
+        </div>
+        `
+    })
+
+    allTask.innerHTML=sum;
+}
+
+renderTask();
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     let task = document.createElement("div");
+//     task.className="task";
+//     let button = document.createElement("button");
+//     button.innerHTML="Mark as Completed"
+//     let h5 = document.createElement("h5");
+//     h5.innerText=taskInput.value;
+//     task.append(h5, button);
+//     allTask.appendChild(task);
+//     console.log(taskImp.checked);  
+// })
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    currenTasks.push({
+        task: taskInput.value, 
+        details: taskArea.value,
+        imp: taskImp.checked
+    })
+    taskInput.value='';
+    taskArea.value='';
+    taskImp.checked = false;
+    renderTask();
+});
