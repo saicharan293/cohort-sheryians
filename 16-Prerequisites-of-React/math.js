5d501fafc6683c107eb785623e421f380bd74117
saicharan293 <2493saicharan@gmail.com>
2026-05-04 23:36:09 +0530

    imports, exports (default, named)

---
+++ 16-Prerequisites-of-React/math.js
@@ -0,0 +1,11 @@
+// Named exports
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export const PI = 3.14;
---
+++ 16-Prerequisites-of-React/message.js
@@ -0,0 +1,7 @@
+// Default export (main thing)
+export default function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Named export
+export const appName = "Module Learning App";
