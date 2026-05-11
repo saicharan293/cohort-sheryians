ee35b444b5224ef5b24cd59f5f8a4e0d412110de
saicharan293 <2493saicharan@gmail.com>
2026-05-11 21:46:58 +0530

    Intro to component creation and importing

--- 17-React-Start/src/App.jsx
+++ 17-React-Start/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className='box'>
+      <Nav />
         <h2>this is app</h2>
+      <Footer />
     </div>
   )
 }
---
+++ 17-React-Start/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>this is nav</div>
+  )
+}
+
+export default Nav
