5a4c95023e9b63b988dff78fb5d8d7a12ebcb611
saicharan293 <2493saicharan@gmail.com>
2026-05-12 00:34:39 +0530

    Dom notes and worked on hero section of task

--- 17-React-Start/src/App.jsx
+++ 17-React-Start/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Nav from './components/Nav'
 import Section from './components/Section'
 
+//task image
+
+//https://mir-s3-cdn-cf.behance.net/project_modules/2800_webp/3334c3231972499.68933676c1c44.png
+
 const App = () => {
   return (
     <div className='box'>
--- 17-React-Start/src/components/Nav.jsx
+++ 17-React-Start/src/components/Nav.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='nav'>this is nav</div>
+    <section className='nav'>
+        <div className="left">
+            <h2>React Space</h2>
+        </div>
+        <div className="middle">
+            <a href="" className='about'>About us</a>
+            <a href="">Services</a>
+            <a href="">Coaches</a>
+            <a href="">Events</a>
+            <a href="">Contacts</a>
+        </div>
+        <div className="right">
+            <button> Book Now </button>
+        </div>
+    </section>
   )
 }
 
