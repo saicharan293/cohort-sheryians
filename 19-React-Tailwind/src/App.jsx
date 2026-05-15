71e44fdeea2b5d12da965383cdee38ca398cf675
saicharan293 <2493saicharan@gmail.com>
2026-05-15 08:50:39 +0530

    intro to tailwind css in React Js

---
+++ 19-React-Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <h2 className='text-white'>this is using tailwindcss</h2>
+    </div>
+  )
+}
+
+export default App
