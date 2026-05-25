debad90e6ff642fe320af711ad0abaaf8382e7f2
saicharan293 <2493saicharan@gmail.com>
2026-05-25 19:23:19 +0530

    Function call with paramter

--- 22-React-Fn-Calling/src/App.jsx
+++ 22-React-Fn-Calling/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const App = () => {
-  const btnClick = () =>{
-    console.log("abcd");
+  const btnClick = (a) =>{
+    console.log("number is", a);
   }
 
   return (
     <div className='bg-black text-white p-5 h-screen'>
       <button 
-        onClick={btnClick}
+        onClick={()=>{
+          btnClick(10)
+        }}
         className='active:scale-95 bg-emerald-900 px-4 py-2 rounded'>
           Click to Download
       </button>
