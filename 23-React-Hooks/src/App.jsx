bfbf2833b0c0b61bec929681825534b7eeaa781d
saicharan293 <2493saicharan@gmail.com>
2026-05-29 23:05:03 +0530

    radio buttons in react

--- 23-React-Hooks/src/App.jsx
+++ 23-React-Hooks/src/App.jsx
@@ -1,25 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  
-  const [marks, setMarks] = useState([50, 72, 10, 12, 16]);
-
-  function graceStudent(){
-    let nmarks = marks.map(function(ele){
-      return ele < 33 ? ele+5 : ele;
-    })
-    
-    setMarks(nmarks)
-  }
-
-
   return (
-    <div>
-      {marks.map(function(e, idx){
-        return <h2 key={idx}>student {idx+1} : {e}({e <33?'Fail':'Pass'})</h2>
-      })}
-
-      <button onClick={graceStudent}>Give them grace</button>
+    <div className='parent'>
+      <div>
+        <input type="radio" name='gender' id='male' />
+        <label htmlFor="male"> Male</label>
+      </div>
+      <div>
+        <input type='radio' name='gender' id='female' />
+        <label htmlFor="female"> FeMale</label>
+      </div>
     </div>
   )
 }
