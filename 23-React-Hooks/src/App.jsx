49a0736ce277792d5f760094cc3f1602e2119266
saicharan293 <2493saicharan@gmail.com>
2026-05-29 19:13:32 +0530

    Grace marks to pass

--- 23-React-Hooks/src/App.jsx
+++ 23-React-Hooks/src/App.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const arr = ['Sai', 'Karthik', 'Dhananjay','Chandan','Abhi'];
 
-  const [num, setNum] = useState(0);
-  function ChangeUser(){
-    if(num < arr.length-1) setNum(num+1);
-    else setNum(0)
+
+  const [marks, setMarks] = useState([50, 72, 10, 12, 16]);
+  const [status, setStatus] = useState('Pass');
+
+  function graceStudent(){
+    console.log('grace given');
+    let nmarks = marks.map(function(ele){
+      return ele < 33 ? ele+5 : ele;
+    })
+    
+    setMarks(nmarks)
   }
+
+
   return (
     <div>
-      <h1> {arr[num]}</h1>
-      <button onClick={ChangeUser}>Change User</button>
+      {marks.map(function(e, idx){
+        return <h2 key={idx}>student {idx+1} : {e}({e <33?'Fail':'Pass'})</h2>
+      })}
+
+      <button onClick={graceStudent}>Give them grace</button>
     </div>
   )
 }
