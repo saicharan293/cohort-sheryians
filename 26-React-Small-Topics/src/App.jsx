de04106ba05e3c56cbdf16a58dafceba4d0b97e1
saicharan293 <2493saicharan@gmail.com>
2026-09-22 00:41:57 +0530

    React Fragments

---
+++ 26-React-Small-Topics/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Top is fragment "<></>"</h1>
+      <p>To Group multiple elements with any extra html element</p>
+    </>
+  )
+}
+
+export default App
