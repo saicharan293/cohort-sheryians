310e01df8563b5da5404fd978f2fb9b8052b30bf
saicharan293 <2493saicharan@gmail.com>
2026-09-22 08:40:23 +0530

    Prop drilling and local storage notes

--- 26-React-Small-Topics/src/App.jsx
+++ 26-React-Small-Topics/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
+
+  const cardData = {
+    username: "Shiva",
+    role: "Creator",
+    email: "shiva@gmail.com",
+    profileUrl: "https://images.unsplash.com/photo-1789303225087-8307ece66da3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"
+  }
   return (
     <>
-      <h1>Top is fragment "<></>"</h1>
-      <p>To Group multiple elements with any extra html element</p>
+      <Card cardData={cardData}/>
     </>
   )
 }
